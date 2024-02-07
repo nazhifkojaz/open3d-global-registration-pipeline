4b281f4401e6c28c2ede3a67275b2d9568958ac0
nazhifkojaz <kojazkojaz@gmail.com>
2024-02-07 11:40:12 +0900

    Added more test cases for preprocessing and registration

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,20 +1,21 @@
 import open3d as o3d
 import copy
 import pytest
+import numpy as np
 from pcd_register.preprocessing import preprocess_pointcloud, downsample, compute_features
 
 
 @pytest.fixture
-def source_pointcloud():
+def load_pointcloud():
     return o3d.io.read_point_cloud(o3d.data.DemoICPPointClouds().paths[0])
 
 @pytest.fixture
 def target_pointcloud():
     return o3d.io.read_point_cloud(o3d.data.DemoICPPointClouds().paths[1])
 
-def test_preprocess_pointcloud(source_pointcloud, target_pointcloud):
-     # Create a sample point cloud for testing
-    pointcloud = o3d.io.read_point_cloud(o3d.data.DemoICPPointClouds().paths[0])
+def test_preprocess_pointcloud(load_pointcloud):
+    # Load a sample pointcloud from demo data
+    pointcloud = load_pointcloud
     pointcloud_temp = copy.deepcopy(pointcloud)
     pointcloud_downsampled, pointcloud_features = preprocess_pointcloud(pointcloud_temp, voxel_size=0.05)
 
@@ -28,3 +29,21 @@ def test_preprocess_pointcloud(source_pointcloud, target_pointcloud):
     assert pointcloud_features is not None
     assert isinstance(pointcloud_features, o3d.pipelines.registration.Feature)
     assert pointcloud_temp is not pointcloud
+
+def test_downsample_consistency(load_pointcloud):
+    pointcloud_downsampled_1 = downsample(load_pointcloud)
+    pointcloud_downsampled_2 = downsample(load_pointcloud)
+
+    assert np.allclose(pointcloud_downsampled_1.points, pointcloud_downsampled_2.points)
+
+def test_compute_features_dimension(load_pointcloud):
+    pointcloud = load_pointcloud
+    pointcloud_features = compute_features(pointcloud)
+
+    assert pointcloud_features.data.shape[0] == 33
+
+def test_compute_features_consistency(load_pointcloud):
+    pointcloud_fpfh_1 = compute_features(load_pointcloud)
+    pointcloud_fpfh_2 = compute_features(load_pointcloud)
+
+    assert np.allclose(pointcloud_fpfh_1.data, pointcloud_fpfh_2.data)
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -24,9 +24,30 @@ def test_rough_registration(source_pointcloud, target_pointcloud):
 
     result = rough_registration(source, target, source_features, target_features)
     
-    # assert that the output is an instance of RegistrationResult
+    # assert that the output is not None and an instance of RegistrationResult
     assert result is not None
     assert isinstance(result, o3d.pipelines.registration.RegistrationResult)
+
+def test_rough_registration_invalid_input(source_pointcloud, target_pointcloud):
+    # Extract source and target point clouds and their features from fixtures
+    source, source_features = source_pointcloud
+    target, target_features = target_pointcloud
+
+    # Test failure case when invalid input is provided
+    with pytest.raises(TypeError):
+        rough_registration(None, target, source_features, target_features)
+
+    with pytest.raises(TypeError):
+        rough_registration(source, None, source_features, target_features)
+
+    with pytest.raises(TypeError):
+        rough_registration(source, target, None, target_features)
+
+    with pytest.raises(TypeError):
+        rough_registration(source, target, source_features, None)
+
+    with pytest.raises(TypeError):
+        rough_registration(source, target, source_features, target_features, voxel_size=None)
 
 def test_fine_registration(source_pointcloud, target_pointcloud):
     # Extract source and target point clouds and their features from fixtures
@@ -39,3 +60,23 @@ def test_fine_registration(source_pointcloud, target_pointcloud):
     # assert that the output is an instance of RegistrationResult
     assert result is not None
     assert isinstance(result, o3d.pipelines.registration.RegistrationResult)
+
+def test_fine_registration_invalid_input(source_pointcloud, target_pointcloud):
+    # Extract source and target point clouds and their features from fixtures
+    source, source_features = source_pointcloud
+    target, target_features = target_pointcloud
+
+    result_ransac = rough_registration(source, target, source_features, target_features)
+
+    # Test failure case when invalid input is provided
+    with pytest.raises(TypeError):
+        fine_registration(None, target, result_ransac)
+
+    with pytest.raises(TypeError):
+        fine_registration(source, None, result_ransac)
+
+    with pytest.raises(AttributeError):
+        fine_registration(source, target, None)
+
+    with pytest.raises(TypeError):
+        fine_registration(source, target, result_ransac, voxel_size=None)
